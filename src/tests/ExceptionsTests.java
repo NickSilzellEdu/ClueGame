d7c7b127636c015641dfcdf283982f04657818f0
Andrew Grimes <andrewjgrimes05@gmail.com>
2025-03-03 21:59:17 -0700

    Update ExceptionsTests.java

--- src/tests/ExceptionsTests.java
+++ src/tests/ExceptionsTests.java
@@ -1,5 +1,40 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+import clueGame.BadConfigFormatException;
+import clueGame.Board;
+import java.io.FileNotFoundException;
+
 public class ExceptionsTests {
 
+    @Test
+    public void testBadColumns() throws BadConfigFormatException, FileNotFoundException {
+        assertThrows(BadConfigFormatException.class, () -> {
+            Board board = Board.getInstance();
+            board.setConfigFiles("ClueLayoutBadColumns.csv", "ClueSetup.txt");
+            board.loadSetupConfig();
+            board.loadLayoutConfig();
+        });
+    }
+
+    @Test
+    public void testBadRoom() throws BadConfigFormatException, FileNotFoundException {
+        assertThrows(BadConfigFormatException.class, () -> {
+            Board board = Board.getInstance();
+            board.setConfigFiles("ClueLayoutBadRoom.csv", "ClueSetup.txt");
+            board.loadSetupConfig();
+            board.loadLayoutConfig();
+        });
+    }
+
+    @Test
+    public void testBadRoomFormat() throws BadConfigFormatException, FileNotFoundException {
+        assertThrows(BadConfigFormatException.class, () -> {
+            Board board = Board.getInstance();
+            board.setConfigFiles("ClueLayout.csv", "ClueSetupBadFormat.txt");
+            board.loadSetupConfig();
+            board.loadLayoutConfig();
+        });
+    }
 }
